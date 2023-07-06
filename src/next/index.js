7706d4d12daa851debb8a0ba3a5136c3bdd0e2b5
soxg <jdgray0224@gmail.com>
2023-07-05 20:46:20 -0400

    handler prototype functionality

--- src/next/index.js
+++ src/next/index.js
@@ -1,6 +1,65 @@
 const logData = require("../dataLogger");
 
+class Handler {
+  constructor() {
+    this.durations = [];
+  }
+
+  addDuration(duration) {
+    this.durations.push(duration);
+  }
+
+  average() {
+    const sum = this.durations.reduce((a, b) => a + b, 0);
+    return this.durations.length ? (sum / this.durations.length).toFixed(0) : 0;
+  }
+
+  median() {
+    if (!this.durations.length) return 0;
+    const sorted = [...this.durations].sort((a, b) => a - b);
+    const mid = Math.floor(sorted.length / 2);
+    return sorted.length % 2 !== 0 ? sorted[mid] : (sorted[mid - 1] + sorted[mid]) / 2;
+  }
+
+  min() {
+    return Math.min(...this.durations);
+  }
+
+  max() {
+    return Math.max(...this.durations);
+  }
+
+  sum() {
+    return this.durations.reduce((a, b) => a + b, 0);
+  }
+
+  count() {
+    return this.durations.length;
+  }
+
+  stdDev() {
+    const avg = this.average();
+    const squareDiffs = this.durations.map(value => (value - avg) ** 2);
+    const avgSquareDiff = squareDiffs.reduce((a, b) => a + b, 0) / squareDiffs.length;
+    return Math.sqrt(avgSquareDiff)
+  }
+
+  variance() {
+    const stdDevValue = this.stdDev();
+    return (stdDevValue * stdDevValue)
+  }
+  
+  clear() {
+    this.durations = [];
+  }
+}
+
+
 function sawmillNext(handler, config = {}) {
+  if (!sawmill[handler.name]) {
+    sawmill[handler.name] = new Handler();
+  }
+
     const { precision = 'milliseconds', method = 'console' } = config;
 
     return async function (req, res) {
@@ -8,6 +67,7 @@ function sawmillNext(handler, config = {}) {
       await handler(req, res);
       const endTime = Date.now();
       let duration;
+
   
       switch(precision) {
         case 'seconds':
@@ -18,6 +78,8 @@ function sawmillNext(handler, config = {}) {
           duration = (endTime - startTime).toFixed(0);
           break;
       }
+
+      sawmill[handler.name].addDuration(parseFloat(duration))
 
       let logEntry;
       switch(method){
@@ -35,11 +97,15 @@ function sawmillNext(handler, config = {}) {
             logData(handler.name, logEntry);
             console.log(`Sawmill: ${handler.name}'s request was processed in ${duration} ${precision}`)
             break;
+
       }
   
       console.log(`Sawmill: ${handler.name}'s request processed in ${duration} ${precision}`);
     };
   }
   
-  module.exports = sawmillNext;
+  module.exports = {
+    sawmillNext,
+    sawmill
+  }
   
