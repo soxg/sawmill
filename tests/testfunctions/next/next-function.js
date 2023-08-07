2d52194d7bd94a26d7fec5813713c18877952c92
soxg <jdgray0224@gmail.com>
2023-08-07 12:52:45 -0400

    testing sawmill progress

--- tests/testfunctions/next/next-function.js
+++ tests/testfunctions/next/next-function.js
@@ -1,15 +1,10 @@
-import { sawmillNext } from "../../../src/next"
-
+import sawmill from '../../../src/utils/sawmill'
 async function nextHandler(req, res){
 try {
-   const data = await fetch('https://statsapi.mlb.com/api/v1/stats?stats=Season&group=hitting&season=2022&limit=100&sortStat=slg&hydrate&metrics&fields').json();
-   let result = 1;
-   for (let i = 0; i < data.stats.splits.stat.length; i++) {
-    result = result * i
-   }
+    const data = await fetch('https://statsapi.mlb.com/api/v1/stats?stats=Season&group=hitting&season=2022&limit=100&sortStat=slg&hydrate&metrics&fields').json();
     return data
 }catch(error) {
     console.log(error)
 }
 }  
-export default sawmillNext(nextHandler, {precision: 'milliseconds', method: 'both', version: 'v1'});
+export default sawmill(nextHandler, {mode: 'bothLogs', precision: 'milliseconds'})
--- tests/unit/sawmill.test.js
+++ tests/unit/sawmill.test.js
@@ -1,14 +1,25 @@
-import nextHandler  from '../testfunctions/next/next-function'
-import { sawmill } from '../../src/next/index'
+import wrappedHandler from '../testfunctions/next/next-function';
+import sawmill from '../../src/utils/sawmill'
+// A mock for the fetch function
+global.fetch = jest.fn(() =>
+  Promise.resolve({
+    json: () => Promise.resolve({ some: 'data' }),
+  })
+);
 
 test('Sawmill\'s Basic Functionality', async () => {
-    const data = await nextHandler();
-    expect(data).toBeDefined();
-    expect(data).toMatch('Sawmill');
+  // Mock request and response objects
+  const req = {}; // Customize as needed
+  const res = {}; // Customize as needed
 
-    const average = sawmill.nextHandler.average();
-    expect(average).toBeLessThanOrEqual(19);
-})
+  // Call the wrapped handler, which is the combination of nextHandler and sawmill
+  await wrappedHandler(req, res);
+
+  // Since the handler name might not be directly accessible, you can check the result of your sawmill.stats object if needed
+  // For example:
+  console.log(sawmill.stats['nextHandler'].average());
+});
+
 
 
 test('Sawmill-Next\'s Log Functionality', async () => {
